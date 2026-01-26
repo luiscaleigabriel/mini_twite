749c10f179ca8e83494d36d296f1a7b1ddffbc8e
Luís Silvério Calei Gabriel <132252803+luiscaleigabriel@users.noreply.github.com>
2026-01-26 07:57:16 +0100

    livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Curso Laravel Livewire</title>
+
+    @livewireStyles
 </head>
 <body>
 
+    <div class="container">
+        {{ $slot }}
+    </div>
+
+    @livewireScripts
 </body>
 </html>
