9c4321bdd208bdf36f9ca16808dca4ebc4533bb4
Luís Silvério Calei Gabriel <132252803+luiscaleigabriel@users.noreply.github.com>
2026-01-26 18:33:36 +0100

    ...

--- resources/views/pages/tweets/⚡show.blade.php
+++ resources/views/pages/tweets/⚡show.blade.php
@@ -1,15 +1,34 @@
 <?php
 
 use Livewire\Component;
+use Livewire\WithPagination;
 use App\Models\Tweet;
 
 new class extends Component
 {
+    use WithPagination;
 
+    public string $content;
+    protected $rules = [
+        'content' => 'required|min:3|max:255'
+    ];
 
     public function todos()
     {
-        return Tweet::all();
+        return Tweet::with('user')->paginate(2);
+    }
+
+    public function create()
+    {
+
+        $this->validate();
+
+        Tweet::create([
+            'content' => $this->content,
+            'user_id' => 10,
+        ]);
+
+        $this->content = '';
     }
 
 };
@@ -17,12 +36,27 @@ new class extends Component
 
 <div>
 
+    <h1>Formulário</h1>
+    <form wire:submit.prevent="create" method="post">
+        <input wire:model="content" type="text" name="content" id="content">
+        @error('content') {{ $message }} @enderror
+        <button type="submit">Criar Tweet</button>
+    </form>
+
+    <br>
+    <hr>
+
     <h1>Lista de tweets</h1>
 
     @forelse ($this->todos() as $tweet)
-        <h1>{{ $tweet->user->name }} - {{ $tweet->content }}</h1>
+        <p>{{ $tweet->user->name }} - {{ $tweet->content }}</p>
     @empty
-        <h2>Nenhum tweet encontrado</h2>
+        <p>Nenhum tweet encontrado</p>
     @endforelse
+
+    <hr>
+    <div>
+        {{ $this->todos()->links() }}
+    </div>
 
 </div>
